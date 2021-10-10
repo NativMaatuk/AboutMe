e3f7932d01967dd0be295ae830dce5f0e90b0115
NativMaatuk <nativma22@gmail.com>
2021-10-11 01:13:21 +0300

    fix toggle nav bar
    remove '-bs' from -> data-bs-toggle="collapse" data-bs-target="#navbarColor03"
    and add script in index.html

--- AboutMe/src/app/navbar/navbar.component.ts
+++ AboutMe/src/app/navbar/navbar.component.ts
@@ -8,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   constructor() { }
-
+  flag =false;
   ngOnInit(): void {
+    
   }
-
+  
 }
