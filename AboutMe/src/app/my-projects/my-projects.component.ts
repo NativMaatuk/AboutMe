219eb9be31be24b7c6ee49c6a6b60b47fb60338e
NativMaatuk <nativma22@gmail.com>
2021-10-17 13:06:40 +0300

    improve UI on my-project

--- AboutMe/src/app/my-projects/my-projects.component.ts
+++ AboutMe/src/app/my-projects/my-projects.component.ts
@@ -13,9 +13,9 @@ export class MyProjectsComponent implements OnInit {
     this.projects = [
       {name:'ConfirMe',link:'https://nativmaatuk.github.io/ConfirmMe/',intro:'Web App\ndevelop on Angular',image:'confirmeimg01.png'},
       {name:'Tic-Tac-Toe',link:'https://github.com/NativMaatuk/Tic-Tac-Toe',intro:'PC Game for Windows, Develop on JAVA uses JavaFX, need JDK11 to run.',image:'tic-tac-toeimg01.png'},
-      {name:'Minesweeper',link:'https://github.com/NativMaatuk/Minesweeper',intro:'need to add',image:'Minesweeperimg01.png'},
+      {name:'Minesweeper',link:'https://github.com/NativMaatuk/Minesweeper',intro:'PC Game for Windows, Develop on JAVA uses JavaFX, need JDK11 to run.',image:'Minesweeperimg01.png'},
       {name:'Defects',link:'https://github.com/NativMaatuk/Defects',intro:'The project solution composes of three projects: DefectsViewer, MathActions, UnitTester. develop on C#',image:'Defectsimg01.png'} ,
-      {name:'Simon Says',link:'https://github.com/NativMaatuk/Simon-Says',intro:'Memory Game for Android',image:'Minesweeperimg01.png'}  
+      {name:'Simon Says',link:'https://github.com/NativMaatuk/Simon-Says',intro:'Memory Game for Android',image:'SimonSaysimg01.png'}  
     ];
   }
  
